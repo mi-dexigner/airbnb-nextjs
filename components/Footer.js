b4f09b9b5a9ad8388640977c4f24bdf49946befd
Muhammad idrees <muhammad.idrees@ossols.local>
2021-12-07 20:07:49 +0500

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+const Footer = () => {
+    return (
+        <footer className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+           <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+            </div> 
+           
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNUITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome clone</p>
+            <p>Referrals accepted</p>
+            <p>Papafam</p>
+            </div>  
+           
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>PAPA React</p>
+            <p>Presents</p>
+            <p>Zero to full stack Hero</p>
+            <p>Hundred of Students</p>
+            <p>Join Now</p>
+            </div> 
+
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Centre</p>
+            <p>Trust &amp; Safety</p>
+            <p>Say Hi YouTube</p>
+            <p>Easter Eggs</p>
+            <p>For The Win</p>
+            </div>
+
+           
+        </footer>
+    )
+}
+
+export default Footer
